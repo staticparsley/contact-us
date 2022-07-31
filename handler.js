05782257a9ab26b4947d5a8dec7375eea0a25979
staticparsley <ruben.marin@fox.com>
2022-07-31 17:37:45 -0400

    ses email service completed

--- handler.js
+++ handler.js
@@ -1,18 +1,43 @@
 'use strict';
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+module.exports.createContact = async (event) => {
+  console.log('Recieved:::', event);
+  const { to, from, subject, mesage } = JSON.parse(event.body);
+
+  if(!to || !from || ! subject || !mesage) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: " to or from... are not set properly"})
+    }
+  }
+
+  const params = {
+    Destination: {
+      ToAddress: [to]
+    }, 
+    Message: {
+      Body: {
+        Text: { Data: message }
       },
-      null,
-      2
-    ),
-  };
+      Subject: { Data: subject}
+    },
+    Source: from
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  try {
+    await ses.sendEmail(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: "email sent sucessfully", success: true}),
+    }
+  } catch(error) {
+      console.error(error);
+      return {
+        statusCode: 400,
+        body: JSON.stringify({message: "Email failed to send", success: true}),
+      }
+  }
+
 };
